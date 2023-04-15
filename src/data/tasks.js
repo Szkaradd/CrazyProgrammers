4ac11b1d4aa71777a6dc0e40aa959e6cbf5c653e
Szkaradd <szkaradekmikolaj@gmail.com>
2023-04-16 01:26:28 +0200

    profile from navbar works

---
+++ src/data/tasks.js
@@ -0,0 +1,50 @@
+const TASKS_NUMBER = 15;
+
+class Location {
+  constructor(block_number, sector) {
+    this.block_number = block_number;
+    this.sector = sector;
+  }
+}
+
+class Task {
+  constructor(weight, location, destination, total_distance, package_id) {
+    this.weight = weight;
+    this.location = location;
+    this.destination = destination;
+    this.total_distance = total_distance;
+    this.package_id = package_id;
+  }
+}
+
+export function GetDistance(locA, locB) {
+  var block_distance = Math.abs(locA.block_number - locB.block_number);
+  var sector_distance = Math.abs(locA.sector.localeCompare(locB.sector));
+  return block_distance * 100 + sector_distance * 10;
+}
+
+// get task with random weight(1,30), random location, random destination
+function RandomTask(id) {
+  var weight = Math.floor(Math.random() * 30) + 1;
+  var location = new Location(
+    Math.floor(Math.random() * 5) + 1,
+    String.fromCharCode(Math.floor(Math.random() * 5) + 65)
+  );
+  var destination = new Location(
+    Math.floor(Math.random() * 5) + 1,
+    String.fromCharCode(Math.floor(Math.random() * 5) + 65)
+  );
+  var total_distance = GetDistance(location, destination);
+  var package_id = id;
+  return new Task(weight, location, destination, total_distance, package_id);
+}
+
+function GetTasks(how_much) {
+  var tasks = [];
+  for (var i = 0; i < how_much; i++) {
+    tasks.push(RandomTask(i));
+  }
+  return tasks;
+}
+
+export const tasks = GetTasks(TASKS_NUMBER);
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import { View, Text, Button, StyleSheet, SafeAreaView } from "react-native";
 import AuthContext from "../AuthContext";
+import NavBar from "../components/NavBar";
 
 const Home = ({ navigation }) => {
   const { user, setUser } = useContext(AuthContext);
@@ -11,15 +12,34 @@ const Home = ({ navigation }) => {
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Welcome, {user?.username ?? "guest"}!</Text>
-      <Button title="Logout" onPress={handleLogout} />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.navbar}>
+        <NavBar navigation={navigation} />
+      </View>
+      <View style={styles.content}>
+        <Text style={styles.title}>Welcome, {user?.username ?? "guest"}!</Text>
+        <Button title="Logout" onPress={handleLogout} />
+        <Button
+          title="Go to my profile"
+          onPress={() => navigation.navigate("Profile")}
+        />
+        <Button
+          title="View new task"
+          onPress={() => navigation.navigate("NewTask", { task_details: null })}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+  },
+  navbar: {
+    width: "100%",
+  },
+  content: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
