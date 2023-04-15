406a336c91fcc1f8df754e13b0c5a53e9d529d96
Szkaradd <szkaradekmikolaj@gmail.com>
2023-04-16 01:50:59 +0200

    working drawer and stack navigators and updated app

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import Login from "./src/screens/Login";
 import Home from "./src/screens/Home";
 import AuthContext from "./src/AuthContext";
 import Profile from "./src/screens/Profile";
+import CustomDrawerContent from "./src/components/CustomDrawerContent";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const DrawerNavigator = () => (
+  <Drawer.Navigator
+    drawerContent={(props) => <CustomDrawerContent {...props} />}
+  >
+    <Drawer.Screen name="Home" component={Home} />
+    <Drawer.Screen name="Profile" component={Profile} />
+  </Drawer.Navigator>
+);
 
 const App = () => {
   const [user, setUser] = React.useState(null);
@@ -21,14 +33,9 @@ const App = () => {
             options={{ title: "Login" }}
           />
           <Stack.Screen
-            name="Home"
-            component={Home}
-            options={{ title: "Home" }}
-          />
-          <Stack.Screen
-            name="Profile"
-            component={Profile}
-            options={{ title: "My Profile" }}
+            name="DrawerNavigator"
+            component={DrawerNavigator}
+            options={{ headerShown: false }}
           />
         </Stack.Navigator>
       </NavigationContainer>
